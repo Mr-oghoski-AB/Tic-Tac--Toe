6c72d843e3b3a947f6dcd6f43e002b921ed3cb4f
oghovese imetowa <itzoghuski@gmail.com>
2025-03-02 10:24:41 +0100

    finished the console game

--- script.js
+++ script.js
@@ -19,11 +19,64 @@ const gameBoard = (function (){
         }
         printBoard();
     }
+
+    function winnerCheck () {
+        let column1  = [];
+        let column2 = [];
+        let column3 = [];
+        let row1 = [];
+        let row2 = [];
+        let row3 = [];
+        let leftRightCross  = [board[0].col1, board[1].col2 , board[2].col3];
+        let rightLeftCross = [board[0].col3, board[1].col2 , board[2].col1];
+
+       board.forEach((row, index) => {
+        if (index == 1){
+            row2.push(row.col1, row.col2,row.col3)
+            column1.push(row.col1)
+            column2.push(row.col2)
+            column3.push(row.col3)
+        }
+        if (index == 2){
+            row3.push(row.col1,row.col2,row.col3)
+            column1.push(row.col1)
+            column2.push(row.col2)
+            column3.push(row.col3)
+        }
+        if (index == 0){
+            column1.push(row.col1)
+            column2.push(row.col2)
+            column3.push(row.col3)
+            row1.push(row.col1, row.col2,row.col3)
+        }
+       })
+       let possibleOutcomes = [column1,column2,column3,row1,row2,row3,leftRightCross,rightLeftCross]
+  
+
+        possibleOutcomes.forEach((outcome) => {
+            console.log(outcome);
+            if (outcome.every(item => item === player.player1.value)){
+                console.log('player 1 wins');
+                player.player1.victory = true
+               
+                
+                
+            }
+            else if (outcome.every((item) => item === player.player2.value)){
+                console.log('player 2 wins');
+                player.player2.victory = true
+            }
+        })
+
+        
+    }
     
     return {
         getBoard,
         printBoard,
-        placeMarker
+        placeMarker,
+        winnerCheck,
+        
     }
 })();
 
@@ -33,11 +86,13 @@ const player = (function () {
         const player1 = {
             name : 'player1',
             value : 'X',
+            victory : false
           
         }
         const player2 = {
             name : 'player2',
             value : 'O',
+            victory : false
         
         }
         return {
@@ -70,12 +125,31 @@ const gameControl = (function () {
     }
 
     const playRound = () => {
-        checkPlayerTurn();
-        console.log(`its ${ playerTurn.name}'s turn !`);
-        let row = prompt('choose row');
-        let col = prompt('choose column')
-        gameBoard.placeMarker(row,col,playerTurn.value)
-        
+        let winner ;
+
+        do {
+            checkPlayerTurn();
+            console.log(`its ${ playerTurn.name}'s turn !`);
+            let row = prompt('choose row');
+            let col = prompt('choose column')
+            gameBoard.placeMarker(row,col,playerTurn.value)
+            gameBoard.winnerCheck();
+            console.log(gameBoard.winnerCheck.winner);
+            
+            if (player.player1.victory == true) { winner = player.player1.victory}
+            if (player.player2.victory == true) { winner = player.player2.victory}
+
+        } while (winner == undefined);
+
+        if ( winner !== undefined){
+            // checkPlayerTurn();
+            // console.log(`its ${ playerTurn.name}'s turn !`);
+            // let row = prompt('choose row');
+            // let col = prompt('choose column')
+            // gameBoard.placeMarker(row,col,playerTurn.value)
+            // gameBoard.winnerCheck();
+            console.log('GAME OVER')
+        } 
    
     }
 
